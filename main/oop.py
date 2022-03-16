c075a2c1fad20fe125af22e758b233e13d79d34f
Suril Patel <surilsp@gmail.com>
2022-03-16 15:44:38 -0400

    oop upates

--- main/oop.py
+++ main/oop.py
@@ -1,5 +1,6 @@
 class Person:
-    def __init__(self, name, gender):  # attribute initializer. Object is already created before this method is called
+    # attribute initializer. Object is already created before this method is called
+    def __init__(self, name, gender):
         self.name = name
         self.gender = gender
         self.__secret = None
@@ -28,6 +29,7 @@ print(type(p1))
 print(type(p1) == type(p2))
 print(isinstance(p1, Person))
 print(isinstance(p1, object))
+print(issubclass(Person, object))
 
 
 # class level method and members
@@ -87,6 +89,7 @@ class GraphicShape(ABC):
 
 # g = GraphicShape()
 
+
 class Triangle(GraphicShape):
 
     def __init__(self, base, height):
@@ -96,14 +99,18 @@ class Triangle(GraphicShape):
     def calcArea(self):
         return 0.5 * self.base * self.height
 
-print(Triangle(1,2).calcArea())
+
+print(Triangle(1, 2).calcArea())
 
 # multiple inheritance
+
+
 class A:
     def __init__(self):
         super().__init__()
         self.foo = "foo"
         self.name = "class A"
+
 
 class B:
     def __init__(self):
@@ -111,7 +118,8 @@ class B:
         self.bar = "bar"
         self.name = "class B"
 
-class C(B,A):
+
+class C(B, A):
     def __init__(self):
         super().__init__()
 
@@ -120,15 +128,20 @@ class C(B,A):
         print(self.bar)
         print(self.name)
 
+
+print(help(A))
 C().show_props()
-print(C.mro()) # method resolution order
+print(C.mro())  # method resolution order
 print(C.__mro__)
 
 # interface
+
+
 class JSONify(ABC):
     @abstractmethod
     def toJSON(self):
         pass
+
 
 class GraphicShape(ABC):
     def __init__(self):
@@ -139,6 +152,7 @@ class GraphicShape(ABC):
         pass
 
 # g = GraphicShape()
+
 
 class Triangle(GraphicShape, JSONify):
 
@@ -152,22 +166,23 @@ class Triangle(GraphicShape, JSONify):
     def toJSON(self):
         pass
 
-t = Triangle(1,2)
 
+t = Triangle(1, 2)
 
 
 # using composition
-#has relationship
+# has relationship
 
 class Book:
 
     def __init__(self, author):
         self.author = author
 
+
 class Author:
     def __init__(self, fname, lname):
         self.fname = fname
-        self.lname =lname
+        self.lname = lname
 
 
 # magic methods
@@ -182,7 +197,10 @@ __getattribute__(self, name_attr)  - get attribute plus some logic
 __setattr__(self, name_attr, value) - set attribute plus pre logic
 __getattr__(self, attr_name) - is called with getattribute look up is failed
 __call__(self, attr1, ..) - call the object like function
+__add__(self, other) - custom add
+__len__(self) - custom length
 '''
+
 
 class Magic:
 
@@ -196,11 +214,13 @@ class Magic:
     def __setattr__(self, key, value):
         if key == "name":
             if type(value) is not str:
-                raise ValueError(f"name is not str type but it is {type(value)}")
+                raise ValueError(
+                    f"name is not str type but it is {type(value)}")
         return super().__setattr__(key, value)
 
     def __getattr__(self, item):
         return item + "does not exist"
+
 
 m = Magic("Hello")
 print(m.name)
@@ -210,10 +230,11 @@ x = m.hello
 pass
 
 
-#data classes
+# data classes
 # python 3.7+ type hints are required
 # automatically import repr and eq methods
 from dataclasses import dataclass, field
+
 
 @dataclass
 class Book:
@@ -222,33 +243,78 @@ class Book:
     pages: int
     price: float
 
+
 b = Book("title", "author", 50, 10.0)
 c = Book("title", "author", 50, 10.0)
 print(b == c)
 
 # post init function
 
+
 def price_func():
     return 10
 
+
 @dataclass
 class Book:
-    title: str = "No Title" # default values - non default attr should be first
+    title: str = "No Title"  # default values - non default attr should be first
     author: str = "No AUthor"
     pages: int = 0
     price: float = field(default=0.0)
     price2: float = field(default_factory=price_func)
 
-    def __post_init__(self): # is called after defualt init is called in data class
+    def __post_init__(self):  # is called after defualt init is called in data class
         self.description = f"{self.title}, {self.author}"
 
 # immutable data classes
+
 
 @dataclass(frozen=True)
 class ImmutableClass:
     value1: str = "Value 1"
     value2: str = 0
 
+
 x = ImmutableClass("hello", 15)
 print(x)
-x.value1 = "different"
+
+
+# property decorator
+'''
+Allows your to treat method as property
+'''
+
+
+class Employee:
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    @property
+    def email(self):
+        return f'{self.first}.{self.last}@gmail.com'
+
+    @property
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split()
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        self.first = None
+        self.last = None
+
+
+e = Employee('John', 'Doe')
+print(e.email)
+print(e.fullname)
+e.fullname = 'Michal W'
+print(e.fullname)
+del e.fullname
+print(e.first)
